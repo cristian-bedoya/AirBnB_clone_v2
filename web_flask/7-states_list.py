77fb9b443a8366f0134e4db6f00ef61b239c6f2a
cristian-bedoya <crisbed321@gmail.com>
2021-01-20 17:37:13 +0000

    add task 7 and html

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" starts a Flask web application: """
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """Returns a string at the /states_listt/ route,
+    display a HTML"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def tear_dowm(self):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
